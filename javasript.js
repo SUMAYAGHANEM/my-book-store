70334759ae611f5a59fe3973572bebc2df041255
SUMAYAGHANEM <sumaghanem9@gmail.com>
2021-03-04 03:06:28 -0800

    thank you

---
+++ javasript.js
@@ -0,0 +1,23 @@
+var x = prompt('favorit color');
+    
+if (x=="orange") 
+{
+    alert ("very good")
+}
+ else if (x=="red")
+{   
+    alert ("great")
+} 
+else 
+{
+    alert ("bad")
+}
+
+var y = 30;
+    z = 20;
+    b = y+z
+
+
+    document.write ("sumaya ghanem")
+
+    document.write(Y);
